6c86ab0ce8b84734fd119ba687225bf446c540be
Alex Soares Lopes <as3434126@gmail.com>
2025-08-10 22:48:46 -0300

    add material sobre for e range

---
+++ 7 - Laço for e range()/ex01.py
@@ -0,0 +1,9 @@
+lista = [1, 2, 3, 4]
+contador = 0
+
+for n in lista:
+    print(n)
+
+
+for i in range(0, 11, 2):
+    print(i)
---
+++ 7 - Laço for e range()/main.py
@@ -0,0 +1,22 @@
+"""lista = [10, 20, 30, 30]
+for i in lista:
+    print(i)
+
+
+
+palavra = "Futebal"
+for letra in palavra:
+    print(letra)
+
+
+
+for n in range(1, 6):
+    print(n)"""
+""" 
+nome = input("Digite seu nome: ")
+
+for n in range(10):
+    print(nome, n) """
+
+for x in range(2, 20, 3):
+    print(x)
