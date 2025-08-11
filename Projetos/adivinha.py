112cb99168b5e66a6bbec4df31c2afe6d5e2425b
Alex Soares Lopes <alex.lopes@somosicev.com>
2025-08-11 17:11:25 -0300

    add projeto adivinha

---
+++ Projetos/adivinha.py
@@ -0,0 +1,17 @@
+# adivinha
+import random
+
+num_aleatorio = random.randint(1, 10)
+
+tentativas = 0
+
+while True:
+    tentativa = int(input("digite um número: "))
+    tentativas += 1
+
+    print(f"{tentativa} tentativa feita")
+    if tentativa == num_aleatorio:
+        print(f"Acertou! Número escolhido: {tentativa}")
+        break
+    else:
+        print("Número errado")
