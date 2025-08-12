613b34525bd21375d9b3b37bb1196dc192d4b10e
Alex Soares Lopes <alex.lopes@somosicev.com>
2025-08-12 17:30:07 -0300

    add material sobre manipulação de string

---
+++ 13 - Funções matemáticas built in e módulo Math/main.py
@@ -0,0 +1,21 @@
+import math
+
+
+# valor absoluto (sempre positivo)
+print(abs(-10))
+
+# portência
+print(pow(2, 3))
+
+# máximo e minimo
+print(max(10, 20, 5))
+print(min(2, 10, 3))
+
+# arredondamento com duas casas decimais
+print(round(3.14159, 2))
+
+""" 
+outras funções mtm com math
+consultar a documentação. Pois são funções
+para calculos especificos
+"""
